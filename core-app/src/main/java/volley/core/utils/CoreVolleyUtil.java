11c9ce494ab8d30bf544372033980ceaf77c95d1
Ismail Ceylan <ceylanismail@gmail.com>
2013-12-16 10:36:27 +0100

    Revert "gson is outsourced for advantage/separate usage"
    This reverts commit 66bb8923e654381f798877e07444e772e2f09689.
    
    new reflected method name getter

---
+++ core-app/src/main/java/volley/core/utils/CoreVolleyUtil.java
@@ -0,0 +1,27 @@
+package volley.core.utils;
+
+import com.android.volley.Request;
+
+import java.lang.reflect.Field;
+
+/**
+ * Created by Ismail Ceylan
+ */
+public class CoreVolleyUtil {
+
+    /**
+     * @param methodCode integer value of HTTP Methods
+     * @return String value of HTTP Methods defined in {@link com.android.volley.Request.Method}
+     */
+    public static String getMethodName(int methodCode) throws IllegalAccessException {
+        Field[] fields = Request.Method.class.getDeclaredFields();
+
+        for (Field field : fields) {
+            if (methodCode == field.getInt(null)) {
+                return field.getName();
+            }
+        }
+
+        return null;
+    }
+}
