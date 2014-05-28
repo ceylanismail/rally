3adf2b89ee2505c93cc87e685c008bf7b7dba1e9
Merih Akar <merihakar@gmail.com>
2014-05-28 03:16:52 +0300

    - copyright statement indentation - now unused contentType method of CoreBaseRequest removed

--- rally-lib/src/main/java/io/zeplin/rally/network/CoreBaseRequest.java
+++ rally-lib/src/main/java/io/zeplin/rally/network/CoreBaseRequest.java
@@ -1,18 +1,19 @@
 /*
-* Copyright (C) 2014 Zeplin
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*      http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (C) 2014 Zeplin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.zeplin.rally.network;
 
 import com.android.volley.Request;
@@ -48,11 +49,6 @@ public abstract class CoreBaseRequest<T> {
      * @return body of the action
      */
     protected abstract String body();
-
-    /**
-     * @return content type of the body
-     */
-    protected abstract String contentType();
 
     /**
      * @return headers, unless any authorization is needed
